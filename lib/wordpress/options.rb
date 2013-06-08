69307a42e96a245ba01482373f0203ffe5f94cf2
Keitaroh Kobayashi <keita@kkob.us>
2013-06-08 23:57:21 +0900

    Array and Hash support for WordPress::Options

--- lib/wordpress/options.rb
+++ lib/wordpress/options.rb
@@ -1,19 +1,32 @@
 # Encoding: UTF-8
 
+require 'php_serialize'
+
 class WordPress::Options < WordPress::Base
   def [](key)
     v = nil
     @conn.query("SELECT `option_value` FROM `#{@tbl[:options]}` WHERE `option_name`='#{@conn.escape key}' LIMIT 1").each do |row|
       v = row[:option_value]
     end
+    # Apply out-filters
+    if v
+      if v[0] == 'a' and v[-1] == '}'
+        # PHP-serialized array
+        v = PHP.unserialize v
+      end
+    end
     v
   end
 
   def []=(key, value)
-    old_value = nil
-    @conn.query("SELECT `option_value` FROM `#{@tbl[:options]}` WHERE `option_name`='#{@conn.escape key}' LIMIT 1").each do |row|
-      old_value = row[:option_value]
+    # Apply in-filters
+
+    if value.kind_of?(Hash) or value.kind_of?(Array)
+      value = PHP.serialize value
     end
+
+    old_value = self[key]
+
     if !value.nil? and !old_value.nil? and value != old_value
       # Update operation.
       @conn.query("UPDATE `#{@tbl[:options]}` SET `option_value`='#{@conn.escape value}' WHERE `option_name`='#{@conn.escape key}'")
